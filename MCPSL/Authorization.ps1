94ae5bc231915a05d0a6e809066b005cc09ca9dc
allMagic <116283787+allMagicNB@users.noreply.github.com>
2025-03-24 13:53:20 +0800

    spilt function & simplified code

--- MCPSL/Authorization.ps1
+++ MCPSL/Authorization.ps1
@@ -1,10 +1,18 @@
-function OAuth2AuthCodeFlow {
+function OAuth2AuthorizationCodeRequest {
     param(
-        [string]$ClientID
+        [string]$ClientID,
+        [string]$RedirectURI = "https://login.microsoftonline.com/common/oauth2/nativeclient"
     )
-    Start-Process "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?client_id=$ClientID&response_type=code&redirect_uri=http%3A%2F%2Flocalhost&scope=XboxLive.signin%20offline_access"
-    $JSON = curl -X POST -d "client_id=$ClientID&scope=XboxLive.signin%20offline_access&code=$Code&redirect_uri=http%3A%2F%2Flocalhost&grant_type=authorization_code" "https://login.microsoftonline.com/consumers/oauth2/v2.0/token" | ConvertFrom-Json
-    Write-Output $JSON
+    $EncodedRedirectURI = [System.Web.HttpUtility]::UrlEncode($RedirectURI)
+    Write-Output "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize?client_id=$ClientID&response_type=code&redirect_uri=$EncodedRedirectURI&scope=XboxLive.signin%20offline_access"
+}
+
+function OAuth2AuthorizationCodeRedeem {
+    param (
+        [string]$ClientID,
+        [string]$AuthorizationCode
+    )
+    Write-Output $(curl -X POST -d "client_id=$ClientID&scope=XboxLive.signin%20offline_access&code=$AuthorizationCode&redirect_uri=http%3A%2F%2Flocalhost&grant_type=authorization_code" "https://login.microsoftonline.com/consumers/oauth2/v2.0/token" | ConvertFrom-Json)
 }
 
 function OAuth2RefreshToken {
@@ -12,32 +20,28 @@ function OAuth2RefreshToken {
         [string]$ClientID,
         [string]$RefreshToken
     )
-    $JSON = curl -X POST -d "client_id=$ClientID&scope=XboxLive.signin%20offline_access&refresh_token=$RefreshToken&grant_type=refresh_token" "https://login.microsoftonline.com/consumers/oauth2/v2.0/token" | ConvertFrom-Json
-    Write-Output $JSON
+    Write-Output $(curl -X POST -d "client_id=$ClientID&scope=XboxLive.signin%20offline_access&refresh_token=$RefreshToken&grant_type=refresh_token" "https://login.microsoftonline.com/consumers/oauth2/v2.0/token" | ConvertFrom-Json)
 }
 
 function OAuth2DeviceCodeFlow {
     param(
         [string]$ClientID
     )
-    $JSON = curl -X POST -d "client_id=$ClientID&scope=XboxLive.signin%20offline_access&grant_type=device_code" "https://login.microsoftonline.com/consumers/oauth2/v2.0/token" | ConvertFrom-Json
-    Write-Output $JSON
+    Write-Output $(curl -X POST -d "client_id=$ClientID&scope=XboxLive.signin%20offline_access&grant_type=device_code" "https://login.microsoftonline.com/consumers/oauth2/v2.0/token" | ConvertFrom-Json)
 }
 
 function XboxLiveAuth {
     param (
         [string]$AccessToken
     )
-    $JSON = curl -X POST --json '{"Properties":{"AuthMethod":"RPS","SiteName":"user.auth.xboxlive.com","RpsTicket":"d=$AccessToken"},"RelyingParty":"http://auth.xboxlive.com","TokenType":"JWT"}' "https://user.auth.xboxlive.com/user/authenticate" | ConvertFrom-Json
-    Write-Output $JSON
+    Write-Output $(curl -X POST --json '{"Properties":{"AuthMethod":"RPS","SiteName":"user.auth.xboxlive.com","RpsTicket":"d=$AccessToken"},"RelyingParty":"http://auth.xboxlive.com","TokenType":"JWT"}' "https://user.auth.xboxlive.com/user/authenticate" | ConvertFrom-Json)
 }
 
 function XSTSAuth {
     param (
         [string]$XboxLiveToken
     )
-    $JSON = curl -X POST --json '{"Properties":{"SandboxId":"RETAIL","UserTokens":["$XboxLiveToken"]},"RelyingParty":"rp://api.minecraftservices.com/","TokenType":"JWT"}' "https://xsts.auth.xboxlive.com/xsts/authorize" | ConvertFrom-Json
-    Write-Output $JSON
+    Write-Output $(curl -X POST --json '{"Properties":{"SandboxId":"RETAIL","UserTokens":["$XboxLiveToken"]},"RelyingParty":"rp://api.minecraftservices.com/","TokenType":"JWT"}' "https://xsts.auth.xboxlive.com/xsts/authorize" | ConvertFrom-Json)
 }
 
 function LoginWithXbox {
@@ -45,14 +49,12 @@ function LoginWithXbox {
         [string]$UHs,
         [string]$XSTSToken
     )
-    $JSON = curl -X POST --json '{"identityToken":"XBL3.0 x=$UHs;$XSTSToken"}' "https://api.minecraftservices.com/authentication/login_with_xbox" | ConvertFrom-Json
-    Write-Output $JSON
+    Write-Output $(curl -X POST --json '{"identityToken":"XBL3.0 x=$UHs;$XSTSToken"}' "https://api.minecraftservices.com/authentication/login_with_xbox" | ConvertFrom-Json)
 }
 
 function MCStore {
     param (
         [string]$AccessToken
     )
-    $JSON = curl -H "Authorization: Bearer $AccessToken" "https://api.minecraftservices.com/entitlements/mcstore" | ConvertFrom-Json
-    Write-Output $JSON
+    Write-Output $(curl -H "Authorization: Bearer $AccessToken" "https://api.minecraftservices.com/entitlements/mcstore" | ConvertFrom-Json)
 }
